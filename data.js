95d95b0ac69a854c382766cfbffa71f859d4af41
ramis <111607770+ramisoul84@users.noreply.github.com>
2024-02-24 21:35:45 +0300

    first commit

---
+++ data.js
@@ -0,0 +1,17 @@
+const options = [
+  {
+    id: 1,
+    color: "red",
+    desc: "Fucking Red",
+  },
+  {
+    id: 2,
+    color: "Blue",
+    desc: "Fucking Blue",
+  },
+  {
+    id: 3,
+    color: "Green",
+    desc: "Fucking Green",
+  },
+];
---
+++ index.js
@@ -0,0 +1,87 @@
+function load(img) {
+  pannellum.viewer("panorama", {
+    type: "equirectangular",
+    panorama: `./assets/${img}.jpeg`,
+    //preview: `./assets/${img}.jpeg`,
+    //title: `./assets/${img}.jpeg`,
+    //author: "resVR",
+    autoLoad: true,
+    pitch: -12,
+    yaw: 60,
+    hfov: 145,
+    showControls: true,
+    showZoomCtrl: false,
+    /*
+    hotSpots: [
+      {
+        pitch: 14.1,
+        yaw: 1.5,
+        type: "info",
+        text: "Baltimore Museum of Art",
+        URL: "https://artbma.org/",
+      },
+      {
+        pitch: -9.4,
+        yaw: 222.6,
+        type: "info",
+        text: "Art Museum Drive",
+      },
+      {
+        pitch: -0.9,
+        yaw: 144.4,
+        type: "info",
+        text: "North Charles Street",
+      },
+    ],
+    */
+  });
+}
+const panorama = document.getElementById("panorama");
+const home = document.getElementById("home");
+const options = document.getElementById("options");
+function reset() {
+  while (panorama.firstChild) {
+    panorama.firstChild.remove();
+  }
+  panorama.classList.remove("pnlm-container");
+}
+
+document.getElementById("red").addEventListener("click", function () {
+  reset();
+  load("red");
+});
+document.getElementById("blue").addEventListener("click", function () {
+  reset();
+  load("blue");
+});
+document.getElementById("green").addEventListener("click", function () {
+  reset();
+  load("green");
+});
+document.getElementById("logo").addEventListener("click", function () {
+  reset();
+  home.classList.replace("hide", "show");
+  panorama.classList.replace("show", "hide");
+  options.classList.add("hide");
+});
+document.getElementById("start").addEventListener("click", function () {
+  home.classList.replace("show", "hide");
+  panorama.classList.replace("hide", "show");
+  options.classList.remove("hide");
+});
+
+function initial() {
+  home.classList.add("show");
+  panorama.classList.add("hide");
+  options.classList.add("hide");
+}
+
+//initial();
+
+/*
+const options = document.getElementsByClassName("options");
+const para = document.createElement("P");
+const node = document.createTextNode("This is a paragraph.");
+options.appendChild(para);
+console.log(options);
+*/
