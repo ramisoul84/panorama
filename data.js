9baf2c501d74f02c2a33bdf8a2a7a84c44a0408a
ramis <111607770+ramisoul84@users.noreply.github.com>
2024-03-03 22:34:54 +0300

    'new'

--- data.js
+++ data.js
@@ -1,17 +1,69 @@
-const options = [
+export const data = [
   {
-    id: 1,
-    color: "red",
-    desc: "Fucking Red",
+    name: "Berlin",
+    area: 45,
+    options: [
+      {
+        material: "./assets/data/room1/option1/material.png",
+        description: "dark wood",
+      },
+      {
+        material: "./assets/data/room1/option2/material.png",
+        description: "light wood",
+      },
+      {
+        material: "./assets/data/room1/option3/material.png",
+        description: "mate black",
+      },
+      {
+        material: "./assets/data/room1/option4/material.png",
+        description: "mate White",
+      },
+    ],
   },
   {
-    id: 2,
-    color: "Blue",
-    desc: "Fucking Blue",
+    name: "Hanover",
+    area: 66,
+    options: [
+      {
+        material: "./assets/data/room1/option1/material.png",
+        description: "dark wood",
+      },
+      {
+        material: "./assets/data/room1/option2/material.png",
+        description: "light wood",
+      },
+      {
+        material: "./assets/data/room1/option3/material.png",
+        description: "mate black",
+      },
+      {
+        material: "./assets/data/room1/option4/material.png",
+        description: "mate White",
+      },
+      {
+        material: "./assets/data/room1/option4/material.png",
+        description: "mate White",
+      },
+      {
+        material: "./assets/data/room1/option4/material.png",
+        description: "mate White",
+      },
+    ],
   },
   {
-    id: 3,
-    color: "Green",
-    desc: "Fucking Green",
+    name: "Stutgart",
+    area: 32,
+    options: [{ material: "", camera1: "", cmaera2: "" }],
+  },
+  {
+    name: "Hamburg",
+    area: 36,
+    options: [{ material: "", camera1: "", cmaera2: "" }],
+  },
+  {
+    name: "Munchen",
+    area: 24,
+    options: [{ material: "", camera1: "", cmaera2: "" }],
   },
 ];
--- index.js
+++ index.js
@@ -1,87 +1,100 @@
-function load(img) {
-  pannellum.viewer("panorama", {
-    type: "equirectangular",
-    panorama: `./assets/${img}.jpeg`,
-    //preview: `./assets/${img}.jpeg`,
-    //title: `./assets/${img}.jpeg`,
-    //author: "resVR",
-    autoLoad: true,
-    autoRotate: -2,
-    pitch: -12,
-    yaw: 60,
-    hfov: 145,
-    /*
-    hotSpots: [
-      {
-        pitch: 14.1,
-        yaw: 1.5,
-        type: "info",
-        text: "Baltimore Museum of Art",
-        URL: "https://artbma.org/",
-      },
-      {
-        pitch: -9.4,
-        yaw: 222.6,
-        type: "info",
-        text: "Art Museum Drive",
-      },
-      {
-        pitch: -0.9,
-        yaw: 144.4,
-        type: "info",
-        text: "North Charles Street",
-      },
-    ],
-    */
+import { data } from "./data.js";
+
+// Global Variables
+let selectedCard = 0;
+let selectedOption = 0;
+let selectedCamera = 0;
+
+// Get HtML Elements
+const logo = document.getElementById("logo");
+const label = document.getElementById("label");
+const cards = document.getElementById("cards");
+const options = document.getElementById("options");
+const panorama = document.getElementById("panorama");
+
+//
+function initialCards(data) {
+  data.map((e, i) => {
+    const card = document.createElement("div");
+    card.classList.add("card");
+    card.addEventListener("click", showCard);
+    card.style.backgroundImage = `url("./assets/data/room${i + 1}/bg.jpg")`;
+    card.setAttribute("cardId", i);
+    const name = document.createElement("p");
+    name.classList.add("name");
+    const nameText = document.createTextNode(e.name);
+    name.appendChild(nameText);
+    const area = document.createElement("p");
+    area.classList.add("area");
+    const areaText = document.createTextNode(e.area + " sq.m");
+    area.appendChild(areaText);
+    card.appendChild(name);
+    card.appendChild(area);
+    cards.appendChild(card);
+  });
+  console.log("Cards are initilized");
+}
+//
+function showCard(e) {
+  cards.style.display = "none";
+  logo.classList.replace("logo-home", "logo-view");
+  label.classList.replace("label-home", "label-view");
+  document
+    .getElementsByClassName("logo-view")[0]
+    .addEventListener("click", () => goHome());
+
+  selectedCard = Number(e.target.getAttribute("cardId"));
+  initialOptions(data[selectedCard].options);
+  console.log(data[selectedCard].options);
+}
+//
+function goHome() {
+  cards.style.display = "grid";
+  logo.classList.replace("logo-view", "logo-home");
+  label.classList.replace("label-view", "label-home");
+  while (options.firstChild) {
+    options.removeChild(options.lastChild);
+  }
+  panorama.classList.replace("show", "hide");
+}
+//
+function initialOptions(data) {
+  while (options.firstChild) {
+    options.removeChild(options.lastChild);
+  }
+  data.map((e, i) => {
+    const option = document.createElement("img");
+    option.classList.add("option");
+    option.setAttribute("src", e.material);
+    option.addEventListener("click", () => {
+      let src = `./assets/data/room${selectedCard + 1}/option${
+        i + 1
+      }/camera1.jpeg`;
+      panorama.classList.replace("show", "hide");
+
+      viewPanorama(src);
+      panorama.classList.replace("hide", "show");
+    });
+    options.appendChild(option);
   });
 }
-const panorama = document.getElementById("panorama");
-const home = document.getElementById("home");
-const options = document.getElementById("options");
-function reset() {
-  while (panorama.firstChild) {
-    panorama.firstChild.remove();
-  }
-  panorama.classList.remove("pnlm-container");
+//
+function viewPanorama(src) {
+  pannellum.viewer("panorama", {
+    type: "equirectangular",
+    panorama: src,
+    autoLoad: true,
+    showZoomCtrl: false,
+    autoRotate: -2,
+    pitch: -12,
+    yaw: 280,
+    hfov: 145,
+  });
 }
 
-document.getElementById("red").addEventListener("click", function () {
-  reset();
-  load("red");
-});
-document.getElementById("blue").addEventListener("click", function () {
-  reset();
-  load("blue");
-});
-document.getElementById("green").addEventListener("click", function () {
-  reset();
-  load("green");
-});
-document.getElementById("logo").addEventListener("click", function () {
-  reset();
-  home.classList.replace("hide", "show");
-  panorama.classList.replace("show", "hide");
-  options.classList.add("hide");
-});
-document.getElementById("c1_text").addEventListener("click", function () {
-  home.classList.replace("show", "hide");
-  panorama.classList.replace("hide", "show");
-  options.classList.remove("hide");
-  load("red");
-});
-
-function initial() {
-  home.classList.add("show");
-  panorama.classList.add("hide");
-  options.classList.add("hide");
+//
+function main(data) {
+  initialCards(data);
 }
-
-//initial();
-
-/*
-const options = document.getElementsByClassName("options");
-const para = document.createElement("P");
-const node = document.createTextNode("This is a paragraph.");
-options.appendChild(para);
-console.log(options);
-*/
+//
+main(data);
